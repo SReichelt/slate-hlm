30f8d37b485a2bbe81e8b2a0ce5e6261c59532da
Sebastian Reichelt <SebastianR@gmx.de>
2020-08-23 01:57:37 +0200

    Correction.

--- .static/build.sh
+++ .static/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
-#curl -o .static/buildPreload.js https://slate-prover.org/download/static/buildPreload.js
+curl -o .static/buildPreload.js https://slate-prover.org/download/static/buildPreload.js
 node .static/buildPreload.js Library.slate .static/dist/preload
